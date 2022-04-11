804430c04b7ee5879b859e6cee80bfa8298a3529
94toman <94toman@gmail.com>
2022-04-11 21:38:44 +0200

    changing port to 3001

--- server.js
+++ server.js
@@ -39,6 +39,6 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 // API call to Clarifai API
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 3001, () => {
     console.log(`app is running on port ${process.env.PORT}`);
 })
